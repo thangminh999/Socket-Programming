9664eb3ea6bf2bbace19a54e265c15c18417a3d9
thangminh999 <baothang2001@gmail.com>
2022-08-30 12:34:26 +0700

    30/08/2022_12:34PM

---
+++ Lab02_2011438_HoangNgocMinhThang_MMT/Lab02_Client/Lab02_Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02_Client
+{
+    internal class Program
+    {
+        private const int BUFFER_SIZE = 1024;
+        static void Main(string[] args)
+        {
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Dang ket noi voi server...");
+            serverSocket.Connect(serverEndPoint);
+            if (serverSocket.Connected)
+            {
+                byte[] buff = new byte[BUFFER_SIZE];
+                Console.WriteLine("Ket noi thanh cong voi server ...");
+                int byteReceive = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                string str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                Console.WriteLine(str);
+                Console.ReadKey();
+                try
+                {
+                    serverSocket.Connect(serverEndPoint);
+                }
+                catch (SocketException)
+                {
+                    Console.WriteLine("Khong the ket noi den server");
+                    return;
+                }
+                while (true)
+                {
+                    str = Console.ReadLine();
+                    buff = Encoding.ASCII.GetBytes(str);
+                    serverSocket.Send(buff, 0, buff.Length, SocketFlags.None);
+                    buff = new byte[1024];
+                    byteReceive = serverSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                    str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                    Console.WriteLine(str);
+                }
+
+            }
+        }
+    }
+}
---
+++ Lab02_2011438_HoangNgocMinhThang_MMT/Lab02_Server/Lab02_Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Lab02_Server
+{
+    internal class Program
+    {
+        private const int BUFFER_SIZE = 1024;
+        static void Main(string[] args)
+        {
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            serverSocket.Bind(serverEndPoint);
+            serverSocket.Listen(10);
+            Socket clientSocket = serverSocket.Accept();
+            EndPoint clientEndPoint = clientSocket.RemoteEndPoint;
+            Console.WriteLine(clientEndPoint.ToString());
+            byte[] buff;
+            string hello = "Xin Chao Client !";
+            buff = Encoding.ASCII.GetBytes(hello);
+            clientSocket.Send(buff, 0, buff.Length, SocketFlags.None);
+            Console.ReadKey();
+            while (true)
+            {
+                buff = new byte[1024];
+                int byteReceive = clientSocket.Receive(buff, 0, buff.Length, SocketFlags.None);
+                string str = Encoding.ASCII.GetString(buff, 0, byteReceive);
+                Console.WriteLine(str);
+                clientSocket.Send(buff, 0, byteReceive, SocketFlags.None);
+            }
+
+        }
+    }
+}
