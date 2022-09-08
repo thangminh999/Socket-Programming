3a5eb0d9aa5c38b82d739affad7363bde4b6030b
thangminh999 <baothang2001@gmail.com>
2022-09-09 01:04:18 +0700

    09/09/2022_1:04SA

---
+++ Lab03_2011438_HoangNgocMinhThang_MMT/Lab03_Client/Lab03_Client/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] buff = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            string welcome = "Hello server";
+            buff = Encoding.ASCII.GetBytes(welcome);
+            serverSocket.SendTo(buff, buff.Length, SocketFlags.None, ipep);
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)sender;
+            buff = new byte[1024];
+            int recv = serverSocket.ReceiveFrom(buff, ref Remote);
+            Console.WriteLine("Dang goi cau chao len Server...", Remote.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(buff, 0, recv));
+            //
+            string input, stringData;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input == "exit")
+                    break;
+                serverSocket.SendTo(Encoding.ASCII.GetBytes(input), Remote);
+                buff = new byte[1024];
+                recv = serverSocket.ReceiveFrom(buff, ref Remote);
+                stringData = Encoding.ASCII.GetString(buff, 0, recv); Console.WriteLine(stringData);
+            }
+            Console.WriteLine("Dang dong client");
+            Console.ReadLine();
+            //
+            serverSocket.Close();
+        }
+    }
+}
---
+++ Lab03_2011438_HoangNgocMinhThang_MMT/Lab03_Server/Lab03_Server/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab03_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int recv;
+            byte[] buff = new byte[1024];
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 5000);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverSocket.Bind(ipep);
+            Console.WriteLine("Dang cho Client ket noi den...");
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Remote = (EndPoint)(sender);
+            recv = serverSocket.ReceiveFrom(buff, ref Remote);
+            Console.WriteLine("Da ket noi voi client: {0}", Remote.ToString());
+            Console.WriteLine(Encoding.ASCII.GetString(buff, 0, recv));
+            string welcome = "Da goi cau chao len Server...";
+            buff = Encoding.ASCII.GetBytes(welcome);
+            serverSocket.SendTo(buff, buff.Length, SocketFlags.None, Remote);
+            //
+            while (true)
+            {
+                buff = new byte[1024];
+                recv = serverSocket.ReceiveFrom(buff, ref Remote);
+                Console.WriteLine(Encoding.ASCII.GetString(buff, 0, recv));
+                serverSocket.SendTo(buff, recv, SocketFlags.None, Remote);
+            }
+        }
+    }
+}
